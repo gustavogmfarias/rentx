d8542ba74dfed7e7eada6b15af9679272022eebd
GUSTAVO GOULART DE MELLO FARIAS <53410368+gustavogmfarias@users.noreply.github.com>
2022-04-19 11:23:48 -0300

    .

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { StatusBar } from "react-native";
 
-import { Container, Title } from "./styles";
+import { Container, Header } from "./styles";
+import Logo from "../../assets/logo.svg";
 
-export function Home() {
+export default function Home() {
   return (
     <Container>
-      <Title>Home</Title>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Header>
+        <Logo />
+      </Header>
     </Container>
   );
 }
